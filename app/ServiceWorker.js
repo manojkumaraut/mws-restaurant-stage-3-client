15f8d22c4afbb542ccaccaa7fc6422a4263266d9
Manojkumar <manojkumarj.be@gmail.com>
2018-12-09 09:18:47 +0530

    feat : Commit for the  Offline Reviews with SW

--- app/ServiceWorker.js
+++ app/ServiceWorker.js
@@ -9,14 +9,11 @@ self.addEventListener('install', event => {
     caches.open(staticCacheName)
       .then(cache => {
         return cache.addAll([
+          '/',
           '/index.html',
           '/css/styles.css',
-          '/css/font-awesome.min.css',
-          '/js/dbhelper.js',
-          '/js/registerServiceWorker.js',
-          '/js/main.js',
-          '/js/restaurant_info.js',
-          '/data/restaurants.json',
+          '/js/index.min.js',
+          '/js/restaurant.min.js',
           '/restaurant.html?id=1',
           '/restaurant.html?id=2',
           '/restaurant.html?id=3',
@@ -27,6 +24,16 @@ self.addEventListener('install', event => {
           '/restaurant.html?id=8',
           '/restaurant.html?id=9',
           '/restaurant.html?id=10',
+          '/restaurant.html?id=1&isOffline=true',
+          '/restaurant.html?id=2&isOffline=true',
+          '/restaurant.html?id=3&isOffline=true',
+          '/restaurant.html?id=4&isOffline=true',
+          '/restaurant.html?id=5&isOffline=true',
+          '/restaurant.html?id=6&isOffline=true',
+          '/restaurant.html?id=7&isOffline=true',
+          '/restaurant.html?id=8&isOffline=true',
+          '/restaurant.html?id=9&isOffline=true',
+          '/restaurant.html?id=10&isOffline=true',
           '/img/appoffline.png'
         ]).catch(error => {
           console.log('Caches open failed: ' + error);
@@ -35,7 +42,7 @@ self.addEventListener('install', event => {
   );
 });
 
-
+let i = 0;
 /**
  *   Fetch event and Offline Caching 
  */
@@ -46,11 +53,74 @@ self.addEventListener('fetch', event => {
   if (requestUrl.port === '1337') {
     event.respondWith(idbResponse(request));
   }
+  
+   if (request.url.includes('reviews')) {
+      let id = +requestUrl.searchParams.get('restaurant_id');
+      event.respondWith(idbReviewResponse(request, id));
+    } else {
+      event.respondWith(idbRestaurantResponse(request));
+    }
+  
   else {
     event.respondWith(cacheResponse(request));
   }
 });
 
+let j = 0;
+function idbRestaurantResponse(request, id) {
+  // 1. getAll records from objectStore
+  // 2. if more than 1 rec then return match
+  // 3. if no match then fetch json, write to idb, & return response
+
+  return idbKeyVal.getAll('restaurants')
+    .then(restaurants => {
+      if (restaurants.length) {
+        return restaurants;
+      }
+      return fetch(request)
+        .then(response => response.json())
+        .then(json => {
+          json.forEach(restaurant => {
+            console.log('fetch idb write', ++j, restaurant.id, restaurant.name);
+            idbKeyVal.set('restaurants', restaurant);
+          });
+          return json;
+        });
+    })
+    .then(response => new Response(JSON.stringify(response)))
+    .catch(error => {
+      return new Response(error, {
+        status: 404,
+        statusText: 'my bad request'
+      });
+    });
+}
+
+let k = 0;
+function idbReviewResponse(request, id) {
+  return idbKeyVal.getAllIdx('reviews', 'restaurant_id', id)
+    .then(reviews => {
+      if (reviews.length) {
+        return reviews;
+      }
+      return fetch(request)
+        .then(response => response.json())
+        .then(json => {
+          json.forEach(review => {
+            console.log('fetch idb review write', ++k, review.id, review.name);
+            idbKeyVal.set('reviews', review);
+          });
+          return json;
+        });
+    })
+    .then(response => new Response(JSON.stringify(response)))
+    .catch(error => {
+      return new Response(error, {
+        status: 404,
+        statusText: 'my bad request'
+      });
+    });
+}
 function cacheResponse(request) {
 	return caches.match(request).then(response => {
         return response || fetch(request).then(fetchResponse => {
